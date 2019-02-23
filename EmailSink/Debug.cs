554308f6a329e04d4f7c5136b7ba224035fb184b
James Swineson <github@public.swineson.me>
2019-02-23 17:11:26 +0800

    add a debug method

---
+++ EmailSink/Debug.cs
@@ -0,0 +1,44 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage;
+
+namespace EmailSink
+{
+    public class Entry
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+    }
+    public static class Debug
+    {
+        [FunctionName("Debug")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]
+            HttpRequest req,
+            [Table("Debug")]
+            IAsyncCollector<Entry> tableBinding,
+            ILogger log)
+        {
+            try
+            {
+                await tableBinding.AddAsync(new Entry()
+                {
+                    PartitionKey = "1111",
+                    RowKey = "1111",
+                });
+                await tableBinding.FlushAsync();
+            }
+            catch (StorageException)
+            {
+                // we expect an Exception "The specified entity already exists"
+                return new OkObjectResult("This passes test");
+            }
+
+            return new OkObjectResult("This passes test too");
+        }
+    }
+}
