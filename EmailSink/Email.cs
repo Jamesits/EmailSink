635b7f9ceb83779f3ed84e68e86140a9d8def003
James Swineson <github@public.swineson.me>
2019-02-23 11:32:51 +0800

    store email into table

--- EmailSink/Email.cs
+++ EmailSink/Email.cs
@@ -6,5 +6,33 @@ namespace EmailSink
 {
     public class Email
     {
+        // system-level key
+
+        /// <summary>
+        /// Automatically set to "yyyyMMdd", do not submit it in the request
+        /// </summary>
+        public string PartitionKey { get; set; }
+
+        /// <summary>
+        /// Unique primary key
+        /// </summary>
+        public string RowKey { get; set; }
+
+        // Add your own keys below
+
+        public string ReceivedTime { get; set; }
+        public DateTime HookTime { get; set; }
+        public string From { get; set; }
+        public string InReplyTo { get; set; }
+        public string References { get; set; }
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+        public string UserAgent { get; set; }
+        public int AttachmentCount { get; set; }
+        public string Recipient { get; set; }
+        public string StrippedText { get; set; }
+        public string StrippedSignature { get; set; }
+        public bool isHtml { get; set; }
+        public string Body { get; set; }
     }
 }
--- EmailSink/EmailIngress.cs
+++ EmailSink/EmailIngress.cs
@@ -1,22 +1,32 @@
 using System;
 using System.Net.Http;
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.ApplicationInsights.DataContracts;
 
 namespace EmailSink
 {
     public static class EmailIngress
     {
+        public static DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            // Unix timestamp is seconds past epoch
+            var dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dtDateTime;
+        }
+
         [FunctionName("EmailIngress")]
         public static async System.Threading.Tasks.Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)]
             HttpRequest req,
             [Table("Emails")]
             IAsyncCollector<Email> tableBinding,
+            ExecutionContext context,
             ILogger log
             )
         {
@@ -29,6 +39,12 @@ namespace EmailSink
                     log.LogInformation($"{kvpair.Key} = {kvpair.Value}");
                 }
 
+                // check if the request is coming from Mailgun
+                if (!req.Headers["User-Agent"].ToString().StartsWith("User-Agent"))
+                {
+                    return new BadRequestObjectResult("Unidentifiable content");
+                }
+
                 // parse body
                 log.LogWarning("RequestBody");
                 var stream = new StreamContent(req.Body);
@@ -36,24 +52,87 @@ namespace EmailSink
                     System.Net.Http.Headers.MediaTypeHeaderValue.Parse(req.Headers["Content-Type"]);
                 var result = await stream.ReadAsMultipartAsync();
 
+                var email = new Email()
+                {
+                    PartitionKey = DateTime.UtcNow.Date.ToString("yyyyMMdd"),
+                };
+                var bodyPlain = "";
+                var bodyHtml = "";
+
                 foreach (var part in result.Contents)
                 {
                     var key = part.Headers.ContentDisposition.Name;
                     var value = await part.ReadAsStringAsync();
                     log.LogInformation($"{key} = {value}");
+
+                    switch (key)
+                    {
+                        case "timestamp":
+                            email.HookTime = UnixTimeStampToDateTime(int.Parse(value));
+                            break;
+                        case "Date":
+                            email.ReceivedTime = value;
+                            break;
+                        case "from":
+                            email.From = value;
+                            break;
+                        case "In-Reply-To":
+                            email.InReplyTo = value;
+                            break;
+                        case "Message-Id":
+                            email.RowKey = value;
+                            break;
+                        case "References":
+                            email.References = value;
+                            break;
+                        case "sender":
+                            email.Sender = value;
+                            break;
+                        case "subject":
+                            email.Subject = value;
+                            break;
+                        case "recipient":
+                            email.Recipient = value;
+                            break;
+                        case "user-agent":
+                            email.UserAgent = value;
+                            break;
+                        case "attachment-count":
+                            email.AttachmentCount = int.Parse(value);
+                            break;
+                        case "body-plain":
+                            bodyPlain = value;
+                            break;
+                        case "body-html":
+                            bodyHtml = value;
+                            break;
+                    }
                 }
 
-                // check if the request is coming from Mailgun
-                if (!req.Headers["User-Agent"].ToString().StartsWith("User-Agent"))
+                if (string.IsNullOrWhiteSpace(bodyHtml))
                 {
-                    return new BadRequestObjectResult("Unidentifiable content");
+                    email.Body = bodyPlain;
+                    email.isHtml = false;
+                }
+                else
+                {
+                    email.Body = bodyHtml;
+                    email.isHtml = true;
                 }
 
+                // write to table
+                using (var operation = Telemetry.Client.StartOperation<DependencyTelemetry>("WriteTable"))
+                {
+                    operation.Telemetry.Context.Operation.Id = context.InvocationId.ToString();
+
+                    await tableBinding.AddAsync(email);
+                }
 
                 return new OkObjectResult("Success");
             }
             catch (Exception ex)
             {
+                log.LogError(ex.ToString());
                 Telemetry.Client.TrackException(ex);
 
                 // tell Mailgun to not retry
